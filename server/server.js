0e82f64df5c1eed8627cdff683c2d43c7da2f2d4
Edwin Harmuth <edwin.harmuth@web.de>
2022-08-01 12:49:39 +0200

    delete fetch from server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,6 @@ const app = express();
 
 app.use(express.static("./public"));
 
-const planetData = require("./planetData.json");
-app.get("/api/planets", (req, res) => {
-    res.json(planetData);
-});
-
 app.listen(process.env.PORT || 8080, () => {
     console.log(`I'm listening on PORT ${process.env.PORT || 8080}`);
 });
